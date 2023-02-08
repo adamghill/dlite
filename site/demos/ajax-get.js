a3441ce6f6abaff62799841e1aa68c9733f750a7
adamghill <adamghill@yahoo.com>
2023-02-07 22:17:25 -0500

    Update demo.

--- site/demos/ajax-get.js
+++ site/demos/ajax-get.js
@@ -1,55 +1,37 @@
 // import Dlite from "//unpkg.com/dlite";
-import Dlite from "../../src/index.js";
+import Dlite, { fetcher } from "../../src/index.js";
 
-async function fetching(url) {
-  return await fetch(url)
-    .then((res) => {
-      if (res.status >= 400) {
-        throw new Error(`${res.status}: ${res.url}`);
-      } else {
-        return res;
-      }
-    })
-    .then((res) => {
-      if ((res.headers.get("content-type") || "").includes("json")) {
-        return res.json();
-      }
+const template = await fetcher("components/ajax-get.js.html");
 
-      return res.text();
-    });
-}
-
-// TODO: Handle error and output in html (in "debug mode" only? otherwise to the console?)
-// TODO: Set `this.data` directly
-// TODO: Be able to add keys to `this.data` on the fly
-
-Dlite(
+const components = Dlite(
   [
     {
       tagName: "ajax-get",
-      template: await fetching("components/ajax-get.js.html"),
+      template: template,
     },
     {
       el: "#template-shadow",
-      template: await fetching("components/ajax-get.js.html"),
-      shadowDOM: true,
+      template: template,
     },
     {
       el: "#template-no-shadow",
-      template: await fetching("components/ajax-get.js.html"),
+      template: template,
       shadowDOM: false,
     },
     {
       el: "#in-place-template",
-      shadowDOM: false,
     },
   ],
   {
+    debug: true,
+    shadowDOM: true,
     data: { activity: {} },
     async created() {
       const url = "https://www.boredapi.com/api/activity";
-      const activity = await fetching(url);
+      const activity = await fetcher(url);
       this.data.activity = activity;
     },
   }
 );
+
+console.log("components", components);
