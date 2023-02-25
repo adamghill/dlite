b00b37c7db85f6f04e8ed63de34018f40de7be6f
adamghill <adamghill@yahoo.com>
2023-02-25 17:44:18 -0500

    Cleaner component config for demos.

--- site/demos/ajax-get.js
+++ site/demos/ajax-get.js
@@ -1,76 +1,53 @@
-// import Dlite from "//unpkg.com/dlite";
+// import Dlite, { fetcher } from "//unpkg.com/dlite";
 import Dlite, { fetcher } from "../../src/index.js";
 
 const template = await fetcher("components/ajax-get/ajax-get.js.html");
-
-let components = [];
 
 const formatter = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
 });
 
-// components = Dlite({
-//   el: "#template-shadow",
-//   template: template,
-//   debug: true,
-//   data: { activity: {} },
-//   getPrice() {
-//     if (this.data.activity.price == 0) {
-//       return "Free!";
-//     }
-
-//     return formatter.format(this.data.activity.price);
-//   },
-//   async created() {
-//     const url = "https://www.boredapi.com/api/activity";
-//     const activity = await fetcher(url);
-//     this.data.activity = activity;
-//   },
-// });
-
-components = Dlite(
-  [
-    {
-      tagName: "ajax-get",
-      template: template,
-    },
-    {
-      el: "#template-shadow",
-      template: template,
-    },
-    {
-      el: "#template-no-shadow",
-      template: template,
-      shadowDOM: false,
-    },
-    {
-      el: "#in-place-template",
-    },
-  ],
+const componentConfigurations = [
   {
-    getPrice() {
-      if (this.data.activity.price == 0) {
-        return "Free!";
-      }
+    tagName: "ajax-get",
+    template: template,
+  },
+  {
+    el: "#template-shadow",
+    template: template,
+  },
+  {
+    el: "#template-no-shadow",
+    template: template,
+    shadowDOM: false,
+  },
+  {
+    el: "#in-place-template",
+  },
+];
 
-      return formatter.format(this.data.activity.price);
-    },
-    debug: true,
-    shadowDOM: true,
-    data: { activity: {} },
-    async created() {
-      // const url = "https://www.boredapi.com/api/activity";
-      // const activity = await fetcher(url);
-      // this.data.activity = activity;
+const components = Dlite(componentConfigurations, {
+  getPrice() {
+    if (this.data.activity.price == 0) {
+      return "Free!";
+    } else if (!this.data.activity.price) {
+      return "";
+    }
 
-      const activity = await fetcher("https://www.boredapi.com/api/activity");
+    return formatter.format(this.data.activity.price);
+  },
+  debug: true,
+  shadowDOM: true,
+  data: { activity: {} },
+  async created() {
+    const activity = await fetcher("https://www.boredapi.com/api/activity");
 
-      setTimeout(() => {
-        this.data.activity = activity;
-      }, 500);
-    },
-  }
-);
+    // Simulate network latency
+    setTimeout(() => {
+      this.data.activity = activity;
+    }, 500);
+  },
+});
 
 console.log("components", components);
